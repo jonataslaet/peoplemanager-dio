ad13923a72a52990f26d9b7724ac8d634df1f17d
Jonatas Laet <jonataslaetprogramador@gmail.com>
2021-07-07 04:26:08 -0300

    Implementados os endpoints de update e delete

--- src/main/java/com/digitalinnovation/one/peoplemanagerdio/controllers/PersonController.java
+++ src/main/java/com/digitalinnovation/one/peoplemanagerdio/controllers/PersonController.java
@@ -6,9 +6,11 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,5 +38,15 @@ public class PersonController {
 	@GetMapping
 	public ResponseEntity<List<PersonDTO>> findAll(){
 		return personService.findAll();
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Void> update(@PathVariable("id") Long id, @RequestBody @Valid PersonDTO personDTO){
+		return personService.update(id, personDTO);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> deleteById(@PathVariable("id") Long id){
+		return personService.deleteById(id);
 	}
 }
